2aa3425fae085980d1c95284514b3de58c375e1b
waverew <waverew@gmail.com>
2023-04-25 17:16:57 -0400

    added json, home component. working on pulling data from json

--- Phone_Book_Dotnet_Angular/ClientApp/src/app/app.module.ts
+++ Phone_Book_Dotnet_Angular/ClientApp/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-
+import { AddPhoneComponent } from './add-phone/add-phone.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,6 +26,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'add-phone', component: AddPhoneComponent }
     ])
   ],
   providers: [],
--- Phone_Book_Dotnet_Angular/ClientApp/src/app/home/home.component.ts
+++ Phone_Book_Dotnet_Angular/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public phones: string = "";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+interface PhonebookItem {
+  name: string;
+  familyName: string;
+  number: string;
 }
